69cf23e9c9047681e8272d5288b6534816751b4f
Jhoselin Guachizaca <jaguachizaca@gmail.com>
2019-10-17 12:49:24 -0500

    ejemplo

--- Ejemplo2/src/ejemplo2/Ejemplo2.java
+++ Ejemplo2/src/ejemplo2/Ejemplo2.java
@@ -16,6 +16,16 @@ public class Ejemplo2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String nombre = "Jhoselin Anai";
+        System.out.println(nombre);
+        String apellido = "Guachizaca Vargas";
+        System.out.println(apellido);
+        String ciudad = "Loja";
+        System.out.println(ciudad);
+        String universidad = "Universidad Tecnica Particular de Loja";
+        System.out.println(universidad);
+        
+        
     }
     
 }
